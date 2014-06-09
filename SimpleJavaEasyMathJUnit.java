65ed013798edc49200d2077b489fca312642010d
tcyrus <tim.h.cyrus@gmail.com>
2014-06-09 14:56:00 -0400

    Add JUnit Testing Example

---
+++ SimpleJavaEasyMathJUnit.java
@@ -0,0 +1,22 @@
+import sesf.simplejavaeasymath.*;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleJavaEasyMathJUnit {
+   private SimpleNumber one;
+   private SimpleNumber two;
+   /** Fixture initialization **/
+   @Before public void setUp() {
+      one=new SimpleDouble("1.0");
+      two=new SimpleDouble("2.0");
+   }
+
+   /** A test that checks the addition of 1+1. **/
+   @Test public void defaultTest() {
+      SimpleOperator adder=new SimpleAddition();
+      SimpleResult r=SimpleCalculationRunner.runTwo(adder,one,two);
+      assertEquals(r.getNumber().getDoubleValue(),3.0,1E-6);
+   }
+}
