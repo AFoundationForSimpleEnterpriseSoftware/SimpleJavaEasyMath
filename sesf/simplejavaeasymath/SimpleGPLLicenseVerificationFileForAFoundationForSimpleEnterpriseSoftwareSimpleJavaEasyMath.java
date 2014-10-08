8f7018b7a242fdd27863d683a988916908af8bdb
Timothy Cyrus <tcyrus@users.noreply.github.com>
2014-10-08 09:47:55 -0400

    Create SimpleGPLLicenseVerificationFileForAFoundationForSimpleEnterpriseSoftwareSimpleJavaEasyMath.java

---
+++ sesf/simplejavaeasymath/SimpleGPLLicenseVerificationFileForAFoundationForSimpleEnterpriseSoftwareSimpleJavaEasyMath.java
@@ -0,0 +1,18 @@
+package sesf.simplejavaeasymath;
+
+public class SimpleGPLLicenseVerificationFileForAFoundationForSimpleEnterpriseSoftwareSimpleJavaEasyMath {
+  /**
+     * SimpleGPLLicenseVerificationFileForAFoundationForSimpleEnterpriseSoftwareSimpleJavaEasyMath
+     * verifies that you have accepted the GNU GENERAL PUBLIC LICENSE
+     * @author TCYRUS
+     * @version 1
+     * @since 0.0.1
+  */
+  
+  // By Modifying This Code You Have Accept The GNU GENERAL PUBLIC LICENSE
+  private boolean SimpleGPLLicenseVerificationBoolean = false;
+  
+  public SimpleResult hasAcceptedTheGPLLicense() {
+    return SimpleGPLLicenseVerificationBoolean;
+  }
+}
